1104a974d59ef74b904c8a26baee92893fd26b7d
otuekongakpan <gmaniacs85@gmail.com>
2025-11-11 17:02:09 +0100

    Replace setSectionSelection with populateSelection

--- scripts/modules.mjs
+++ scripts/modules.mjs
@@ -1,5 +1,5 @@
 import byuiCourse from "./course.mjs";
-import { setSectionSelection } from "./sections.mjs";
+import { populateSelection } from "./sections.mjs";
 import { setTitle, renderSections } from "./output.mjs";
 
 
