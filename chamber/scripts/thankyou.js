57b543870e208043a48da58b0da880da8ad91726
otuekongakpan <oakpan2@byupathway.edu>
2025-11-20 17:16:56 +0100

    animation complete

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -3,7 +3,7 @@ const params = new URLSearchParams(window.location.search);
 document.getElementById("firstName").textContent = params.get("fname");
 document.getElementById("lastName").textContent = params.get("lname");
 document.getElementById("email").textContent = params.get("email");
-document.getElementById("mobile").textContent = params.get("phonenumber");
+document.getElementById("phone").textContent = params.get("phonenumber");
 document.getElementById("business").textContent = params.get("business-name");
 document.getElementById("timestamp").textContent = params.get("timestamp");
 
