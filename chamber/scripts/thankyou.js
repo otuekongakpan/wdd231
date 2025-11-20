21842797415aee4bdf6102f6a548f5b5f91fe217
otuekongakpan <oakpan2@byupathway.edu>
2025-11-20 16:05:42 +0100

    thank you page init

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,8 @@
+const params = new URLSearchParams(window.location.search);
+
+document.getElementById("firstName").textContent = params.get("fname");
+document.getElementById("lastName").textContent = params.get("lname");
+document.getElementById("email").textContent = params.get("email");
+document.getElementById("phone").textContent = params.get("phone");
+document.getElementById("businessName").textContent = params.get("businessName");
+document.getElementById("timestamp").textContent = params.get("timestamp");
