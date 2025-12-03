034d779c60a76ef107fbeb1cfa6bf370605ce4fb
Otuekong Akpan <otuakpan05@gmail.com>
2025-12-03 15:52:46 -0800

    final

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -41,4 +41,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     displayInterests(interests);
+
+const messageArea = document.querySelector("#visitor-message");
+
+let lastVisit = localStorage.getItem("lastVisit");
+let now = Date.now();
+
+if (!lastVisit) {
+    messageArea.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+    const millisecondsInADay = 1000 * 60 * 60 * 24;
+    const daysBetween = Math.floor((now - lastVisit) / millisecondsInADay);
+
+    if (daysBetween < 1) {
+        messageArea.textContent = "Back so soon! Awesome!";
+    } else if (daysBetween === 1) {
+        messageArea.textContent = "You last visited 1 day ago.";
+    } else {
+        messageArea.textContent = `You last visited ${daysBetween} days ago.`;
+    }
+}
+
+localStorage.setItem("lastVisit", now);
+
 });
